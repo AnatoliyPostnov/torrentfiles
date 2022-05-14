9faace4a99fb4dd438f842fc797f19758f76aaf5
apostnov <apostnov@telematika.com>
2022-05-14 15:39:18 +0300

    new application

---
+++ src/main/kotlin/com/torrentfiles/torrentfiles/TorrentfilesApplication.kt
@@ -0,0 +1,11 @@
+package com.torrentfiles.torrentfiles
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TorrentfilesApplication
+
+fun main(args: Array<String>) {
+	runApplication<TorrentfilesApplication>(*args)
+}
---
+++ src/test/kotlin/com/torrentfiles/torrentfiles/TorrentfilesApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.torrentfiles.torrentfiles
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TorrentfilesApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
